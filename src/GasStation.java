f917d380f38202cffb2f74e37a24ce4c7caa86c1
Linjun Ouyang <lynjunoyang@gmail.com>
2018-03-26 14:28:33 +0800

    Finished gasstation

--- src/GasStation.java
+++ src/GasStation.java
@@ -1,3 +1,18 @@
 public class GasStation {
-    /** git test 2 */
+   public int canCompleteCircuit(int[] gas, int[] cost) {
+       if (gas.length == 0 || cost.length == 0 || gas.length != cost.length) return -1;
+       int start = 0, total = 0, sum = 0;
+
+       for (int i = 0; i < gas.length; i++) {
+           total += gas[i] - cost[i];
+           if (sum < 0) {
+               start = i;
+               sum = gas[i] - cost[i];
+           } else {
+               sum += gas[i] - cost[i];
+           }
+       }
+
+       return total < 0 ? -1 : start;
+   }
 }
