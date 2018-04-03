9b8acb99188b0bc0498da7df1e6a6aaaf2fa02fc
Linjun Ouyang <lynjunoyang@gmail.com>
2018-04-03 11:08:20 +0800

    Finished 121. Best Time to Buy and Sell Stock

---
+++ src/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,14 @@
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) return 0;
+        int min = prices[0];
+        int profit = 0;
+
+        for (int price: prices) {
+            min = Math.min(min, price);
+            profit = Math.max(profit, price - min);
+        }
+
+        return profit;
+    }
+}
