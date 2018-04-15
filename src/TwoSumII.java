fe5b2961d8e4339e39889a68fb4d58e8f040b6a4
Linjun Ouyang <lynjunoyang@gmail.com>
2018-04-15 16:12:07 +0800

    Finished 167. Two Sum II

---
+++ src/TwoSumII.java
@@ -0,0 +1,27 @@
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        /**
+         * Time complexity: O(n)
+         * Space complexity: O(1)
+         */
+        if (numbers == null || numbers.length < 2) {
+            return new int[]{-1, -1};
+        }
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+}
