97b3d67d3fa80c916d460f4f5135769a13b2d724
Linjun Ouyang <lynjunoyang@gmail.com>
2018-04-16 15:21:51 +0800

    Finished Search Insert Position

---
+++ src/SearchInsertPosition.java
@@ -0,0 +1,29 @@
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (target <= nums[start]) {
+            return start;
+        } else if (target <= nums[end]) {
+            return end;
+        } else {
+            return end + 1;
+        }
+    }
+}
