ea3ad5e8cad23cb710c6eabf1173841629f09f99
Linjun Ouyang <lynjunoyang@gmail.com>
2020-10-19 21:56:42 -0700

    10/19 310

---
+++ src/_0310MinimumHeightTrees.java
@@ -0,0 +1,133 @@
+import java.util.*;
+
+public class _0310MinimumHeightTrees {
+    /**
+     * 1. Brute Force
+     *
+     * Starting from each node in the graph, we treat it as a root to build a tree.
+     * Furthermore, we would like to know the distance between this root node and the rest of the nodes.
+     * The maximum of the distance would be the height of this tree.
+     * (559 Maximum Depth of N-ary Tree)
+     *
+     * Then according to the definition of Minimum Height Tree (MHT),
+     * we simply filter out the roots that have the minimal height among all the trees.
+     *
+     * However, this solution is not efficient, whose time complexity would be O(N^2)
+     * where N is the number of nodes in the tree.
+     * As one can imagine, it will result in Time Limit Exceeded exception in the online judge.
+     *
+     */
+
+
+    /**
+     * 2. Topological Sort (Sort of)
+     *
+     * Reference:
+     * https://leetcode.com/problems/minimum-height-trees/discuss/76055/Share-some-thoughts
+     * https://leetcode.com/problems/minimum-height-trees/solution/
+     *
+     * ? why is it topological-sort-like
+     * generates the order of objects based on their dependencies.
+     *
+     * --
+     * (1) A tree is an undirected graph in which any two vertices are
+     * connected by exactly one path.
+     *
+     * (2) Any connected graph who has n nodes with n-1 edges is a tree.
+     *
+     * (3) The degree of a vertex of a graph is the number of
+     * edges incident to the vertex.
+     *
+     * (4) A leaf is a vertex of degree 1. An internal vertex is a vertex of
+     * degree at least 2.
+     *
+     * (5) A path graph is a tree with two or more vertices that is not
+     * branched at all.
+     *
+     * (6) A tree is called a rooted tree if one vertex has been designated
+     * the root.
+     *
+     * (7) The height of a rooted tree is the number of edges on the longest
+     * downward path between root and a leaf.
+     * ---
+     *
+     * Calling iterator() is constant time.
+     * It is a method call that returns an Iterator instance on the collection you are calling on.
+     *
+     * -----
+     * For the tree-alike graph, the number of centroids is no more than 2.
+     *
+     * 1. If the nodes form a chain, it is intuitive to see that the above statement holds,
+     * which can be broken into the following two cases:
+     * a) If the number of nodes is even, then there would be two centroids.
+     * b) If the number of nodes is odd, then there would be only one centroid.
+     *
+     * 2. Other cases: not a chain, proof by contradiction:
+     * Suppose that we have 3 centroids in the graph,
+     * if we remove all the non-centroid nodes in the graph, then the 3 centroids nodes must form a triangle shape
+     * Because these centroids are equally important to each other, and they should equally close to each other as well.
+     * If any of the edges that is missing from the triangle, then the 3 centroids would be reduced down to a single centroid.
+     * However, the triangle shape forms a cycle which is contradicted to the condition
+     * that there is no cycle in our tree-alike graph.
+     * -----
+     *
+     * Any node that has already been a leaf cannot be the root of a MHT,
+     * because its adjacent non-leaf node will always be a better candidate.
+     *
+     * Time:
+     * graph construction: O(|V| - 1)
+     * get initial leaves: O(|V|)
+     * trimming almost all edges and nodes: O(V + V - 1)
+     * Total: O(V)
+     *
+     * Space:
+     * adjacency list: O(V + V - 1)
+     * Leaves: worst case: one centroid, rest are leaves, O(V - 1)
+     * total: O(V)
+     */
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (n == 1) {
+            res.add(0);
+            return res;
+        }
+
+        List<List<Integer>> adjList = new ArrayList<List<Integer>>();
+        for (int i=0; i<n; i++) {
+            adjList.add(new ArrayList<Integer>());
+        }
+        int[] degree = new int[n];
+        for (int i=0; i<edges.length; i++) {
+            adjList.get(edges[i][0]).add(edges[i][1]);
+            adjList.get(edges[i][1]).add(edges[i][0]);
+            degree[edges[i][0]]++;
+            degree[edges[i][1]]++;
+        }
+
+        Queue<Integer> queue = new ArrayDeque<Integer>();
+        for (int i = 0; i < n; i++)  {
+            if (degree[i] == 1) {
+                queue.offer(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            List leaves = new ArrayList<Integer>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++){
+                int curr = queue.poll();
+                leaves.add(curr);
+                for(int next : adjList.get(curr)) {
+                    degree[next]--;
+                    if (degree[next]==1) {
+                        queue.offer(next);
+                    }
+                }
+            }
+            res = leaves;
+        }
+        return res;
+    }
+
+
+}
--- src/_0399EvaluateDivision.java
+++ src/_0399EvaluateDivision.java
@@ -1,6 +1,38 @@
 import java.util.*;
 
 public class _0399EvaluateDivision {
+    /**
+     * 1. Path search (given two end nodes) in Graph
+     *
+     * This one is BFS, could also be done in DFS:
+     * https://leetcode.com/problems/evaluate-division/solution/
+     *
+     * optimization:
+     * rename neighbor->node neighbor.node->node.id
+     *
+     * consider dfs before query processing?
+     *
+     */
+    class Neighbor {
+        String node;
+        double val;
+
+        public Neighbor(String n, double v) {
+            node = n;
+            val = v;
+        }
+    }
+
+    class Result {
+        double val;
+        boolean isValid;
+
+        public Result(boolean valid, double v) {
+            val = v;
+            isValid = valid;
+        }
+    }
+
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
         Map<String, List<Neighbor>> map = new HashMap<>();
 
@@ -22,12 +54,9 @@ public class _0399EvaluateDivision {
             String y = query.get(1);
             res[i] = -1.0;
             if (map.containsKey(x) && map.containsKey(y)) {
-                Map<String, Boolean> visited = new HashMap<>();
+                Set<String> visited = new HashSet<>();
                 for (String node : map.keySet()) {
-                    visited.put(node, false);
-                }
-                for (String node : visited.keySet()) {
-                    if (!visited.get(node)) {
+                    if (!visited.contains(node)) {
                         Result result = bfs(map, visited, x, y);
                         if (result.isValid) {
                             res[i] = result.val;
@@ -41,24 +70,20 @@ public class _0399EvaluateDivision {
         return res;
     }
 
-    private Result bfs(Map<String, List<Neighbor>> map, Map<String, Boolean> visited, String start, String end) {
-        Deque<String> nodeQueue = new ArrayDeque<>();
-        nodeQueue.offer(start);
-        Deque<Double> valQueue = new ArrayDeque<>();
-        valQueue.offer(1.0);
-        visited.put(start, true);
+    private Result bfs(Map<String, List<Neighbor>> map, Set<String> visited, String start, String end) {
+        Deque<Neighbor> queue = new ArrayDeque<>();
+        queue.offer(new Neighbor(start, 1));
+        visited.add(start);
 
-        while (!nodeQueue.isEmpty()) {
-            String node = nodeQueue.poll();
-            double val = valQueue.poll();
-            for (Neighbor next : map.get(node)) {
+        while (!queue.isEmpty()) {
+            Neighbor node = queue.poll();
+            for (Neighbor next : map.get(node.node)) {
                 if (next.node.equals(end)) {
-                    return new Result(true, val * next.val);
+                    return new Result(true, node.val * next.val);
                 }
-                if (!visited.get(next.node)) {
-                    nodeQueue.offer(next.node);
-                    valQueue.offer(val * next.val);
-                    visited.put(next.node, true);
+                if (!visited.contains(next.node)) {
+                    queue.offer(new Neighbor(next.node, node.val * next.val));
+                    visited.add(next.node);
                 }
             }
         }
@@ -66,23 +91,107 @@ public class _0399EvaluateDivision {
         return new Result(false, -1.0);
     }
 
-    class Neighbor {
-        String node;
-        double val;
-
-        public Neighbor(String n, double v) {
-            node = n;
-            val = v;
+    /**
+     * 2. Union Find
+     *
+     * Reference:
+     * https://leetcode.com/problems/evaluate-division/solution/
+     *
+     * N: number of equations
+     * M: number of queries
+     *
+     * Statement: If M operations, either Union or Find, are applied to N elements,
+     * the total run time is O(M * log*N), where log* is iterated logarithm
+     *
+     * Time: O((M + N) * log*N)
+     * union for each equation: O(N * log*N)
+     * twice find() for each query, total: O(M  * log*N)
+     *
+     *
+     * Space Complexity: O(n)
+     *
+     * Union-Find data structure is O(N) where we maintain a state for each variable.
+     * find() function call stack: O(N).
+     *
+     */
+    class Node {
+        String parent;
+        double weight;
+        public Node(String parent, double weight) {
+            this.parent = parent;
+            this.weight = weight;
         }
     }
 
-    class Result {
-        double val;
-        boolean isValid;
+    public double[] calcEquation2(List<List<String>> equations, double[] values,
+                                 List<List<String>> queries) {
 
-        public Result(boolean valid, double v) {
-            val = v;
-            isValid = valid;
+        HashMap<String, Node> numMap = new HashMap<>();
+
+        // Step 1). build the union groups
+        for (int i = 0; i < equations.size(); i++) {
+            List<String> equation = equations.get(i);
+            double quotient = values[i];
+            union(numMap, equation, quotient);
+        }
+
+        // Step 2). run the evaluation, with "lazy" updates in find() function
+        double[] results = new double[queries.size()];
+        for (int i = 0; i < queries.size(); i++) {
+            List<String> query = queries.get(i);
+            String dividend = query.get(0);
+            String divisor = query.get(1);
+
+            if (!numMap.containsKey(dividend) || !numMap.containsKey(divisor)) {
+                // case 1). at least one variable did not appear before
+                results[i] = -1.0;
+            } else {
+                Node dividendNode = find(numMap, dividend);
+                Node divisorNode = find(numMap, divisor);
+
+                if (!dividendNode.parent.equals(divisorNode.parent)) {
+                    // case 2). the variables do not belong to the same chain/group
+                    results[i] = -1.0;
+                } else {
+                    // case 3). there is a chain/path between the variables
+                    results[i] = dividendNode.weight / divisorNode.weight;
+                }
+            }
+        }
+
+        return results;
+    }
+
+    // uses path compression
+    private Node find(HashMap<String, Node> numMap, String id) {
+        if (!numMap.containsKey(id)) {
+            numMap.put(id, new Node(id, 1.0));
+        }
+
+        Node node = numMap.get(id);
+        // found inconsistency, trigger chain update
+        if (!node.parent.equals(id)) {
+            Node parent = find(numMap, node.parent);
+            numMap.put(id, new Node(
+                    parent.parent, node.weight * parent.weight));
+        }
+
+        return numMap.get(id);
+    }
+
+    // no union by size or rank
+    private void union(HashMap<String, Node> numMap, List<String> equation, Double value) {
+        Node dividend = find(numMap, equation.get(0));
+        Node divisor = find(numMap, equation.get(1));
+
+        // merge the two groups together,
+        // by attaching the dividend group to the one of divisor
+        if (!dividend.parent.equals(divisor.parent)) {
+            // here we only update dividend's parent info (parent and weight)
+            // discrepency between children of parent (which includes dividend) and divisor
+            // will be corrected in future find();
+            numMap.put(dividend.parent, new Node(divisor.parent,
+                    divisor.weight * value / dividend.weight));
         }
     }
 }
